98371f0a152588778d201a514295b644ec915715
wieczorekc18 <wieczorekc18@gmail.com>
2020-06-07 20:58:09 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {console.log(`listening on port ${port}`)});
+
